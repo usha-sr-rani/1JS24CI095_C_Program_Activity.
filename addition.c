1e11afbea9aec7bb13fce55aa1211da5b95b82e8
usha-sr-rani <ushasr2007@gmail.com>
2026-05-16 12:35:43 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include &lt;stdio.h&gt;
+int main()
+{
+printf(&quot;Name: YOUR NAME\n&quot;);
+printf(&quot;USN: YOUR USN\n&quot;);
+printf(&quot;Program: Simple Addition of Two Numbers\n&quot;);
+printf(&quot;----------------------------------------\n&quot;);
+int a, b, sum;
+printf(&quot;Enter two numbers: &quot;);
+scanf(&quot;%d %d&quot;, &amp;a, &amp;b);
+sum = a + b;
+printf(&quot;Sum = %d\n&quot;, sum);
+return 0;
+}
